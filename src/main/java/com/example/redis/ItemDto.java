3ef65dfa5478b32802fb05813d3e10bd471ccc7b
hosu <hojune0904@gmail.com>
2024-08-05 15:20:38 +0900

    RedisTemplate 이용한 레디스 저장 테스트

---
+++ src/main/java/com/example/redis/ItemDto.java
@@ -0,0 +1,1 @@
+package com.example.redis;import lombok.*;import java.io.Serializable;@Getter@Setter@Builder@AllArgsConstructor@NoArgsConstructorpublic class ItemDto implements Serializable {    private String name;    private String description;    private Integer price;}
